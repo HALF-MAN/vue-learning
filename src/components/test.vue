b98d24573d9fef3e2ddf390ee660b8f3e1a65a73
zhangyachong <779868735@qq.com>
2020-05-05 19:21:10 +0800

    init

---
+++ src/components/test.vue
@@ -0,0 +1,31 @@
+<template>
+    <h2 class="red">
+        {{msg}}
+    </h2>
+</template>
+
+<style scoped>
+    .red{
+        color: red;
+    }
+</style>
+
+<script>
+export default {
+    name: 'test',
+    props: {
+        msg:{
+            type: String,
+            default: "test msg"
+        }
+    },
+    methods:{
+
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
